1771c9bb5e636b871257f0caac174d33711a8acc
austriaalvinnoel <austriaalvinnoel@gmail.com>
2026-08-14 06:10:25 +0800

    Add New Jersey service route

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ServicePage from "./pages/ServicePage";
+import NewJerseyComedianPage from "./pages/NewJerseyComedianPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,6 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/new-jersey-comedian" element={<NewJerseyComedianPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
