1d717d18dd2311d4cdd8b0239140c1e86040b114
austriaalvinnoel <austriaalvinnoel@gmail.com>
2026-08-14 06:11:13 +0800

    Load party service page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NewJerseyComedianPage from "./pages/NewJerseyComedianPage";
 import CorporateComedianPage from "./pages/CorporateComedianPage";
+import PartyComedianPage from "./pages/PartyComedianPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
