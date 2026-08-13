05b8905c64a3a0b7a0d9a1f5d6673f41e1e94363
austriaalvinnoel <austriaalvinnoel@gmail.com>
2026-08-14 06:11:53 +0800

    Finalize first service routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NewJerseyComedianPage from "./pages/NewJerseyComedianPage";
 import CorporateComedianPage from "./pages/CorporateComedianPage";
-import PartyComedianPage from "./pages/PartyComedianPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
