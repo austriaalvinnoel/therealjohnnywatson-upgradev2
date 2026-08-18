92c1366724a9f18fce963c8c359c0f2dbff0432b
austriaalvinnoel <austriaalvinnoel@gmail.com>
2026-08-19 01:24:21 +0800

    Add press kit to Johnny Watson homepage

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import BookingOptionsSection from "@/components/BookingOptionsSection";
 import VideosSection from "@/components/VideosSection";
 import ShowsSection from "@/components/ShowsSection";
+import PressKitSection from "@/components/PressKitSection";
 import AboutSection from "@/components/AboutSection";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
@@ -16,6 +17,7 @@ const Index = () => {
         <BookingOptionsSection />
         <VideosSection />
         <ShowsSection />
+        <PressKitSection />
         <AboutSection />
         <ContactSection />
       </main>
